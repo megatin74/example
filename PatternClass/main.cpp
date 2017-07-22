247955ef4424d27ddf88cfaaa9cf914e2c075c19
megatin74 <megatin74@gmail.com>
2017-07-22 17:10:07 +0900

    project 1

--- PatternClass/main.cpp
+++ PatternClass/main.cpp
@@ -1,96 +1,109 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
+#include <string>
 #include <vector>
-#include <tuple>
 using namespace std;
-
-class BaseMenu
-{
-    string title;
-public:
-    BaseMenu(string s):title(s){}
-
-    string getTitle(){return title;}
-
-    virtual void command()=0;
-
-};
-
-class MenuItem : public BaseMenu
-{
-    int id;
-public:
-    MenuItem(string s, int n):BaseMenu(s),id(n){}
-    virtual void command()
-    {
-        cout<<getTitle()<<"메뉴 선택됨"<<endl;
-        getchar();
-    }
-
-};
 
 #define clrscr() system("cls")
 
-class PopupMenu : public BaseMenu
+class BaseMenu
 {
-    vector<BaseMenu*> v;
+	string title;
 public:
-    PopupMenu(string s):BaseMenu(s){}
-    void addMenu(BaseMenu* p){v.push_back(p);}
-#if 1
-    virtual void command()
-    {
-        while(1)
-        {
-            clrscr();
-            int sz =v.size();
-            for(int i=0;i<sz;i++)
-            {
-                cout<<i+1<<"."<<v[i]->getTitle()<<endl;
-            }
+	BaseMenu(string t) : title(t) {}
+	virtual ~BaseMenu() {}
+	string getTitle() const { return title; }
 
-            cout<<sz+1<<". 상위 메뉴로"<<endl;
-            cout<<"메뉴를 선택하세요>>";
-            int cmd;
-            cin>>cmd;
-
-            if(cmd<1 || cmd>sz+1)
-                continue;
-
-            if(cmd=sz+1)
-                break;
-
-            v[cmd-1]->command();
-
-
-
-        }
-
-    }
-
-#endif
-
+	virtual void command() = 0;
 };
 
 
-int main(void)
+class MenuItem : public BaseMenu
 {
-    #if 1
-    PopupMenu* menubar = new PopupMenu("MenuBar");
+	int id;
+public:
+	MenuItem(string t, int n) : BaseMenu(t), id(n) {}
+	virtual void command()
+	{
+		cout << getTitle() << " 선택 되었습니다" << endl;
+		getchar();
+	}
+};
 
-    PopupMenu* p1 = new PopupMenu("화면 설정");
-    PopupMenu* p2 = new PopupMenu("소리 설정");
+class PopupMenu : public BaseMenu
+{
+	vector<BaseMenu*> v;
+public:
+	PopupMenu(string s) : BaseMenu(s) {}
 
-    menubar->addMenu(p1);
-    menubar->addMenu(p2);
+	~PopupMenu()
+	{
+		for (auto p : v)
+			delete p;
+	}
 
-    p1->addMenu(new MenuItem("해상도 변경",11));
-    p1->addMenu(new MenuItem("색상 변경",12));
-    p1->addMenu(new MenuItem("명도 변경",13));
-    p2->addMenu(new MenuItem("크기 변경",21));
-    p2->addMenu(new MenuItem("음색 변경",22));
+	void addMenu(BaseMenu* p) { v.push_back(p); }
 
-    menubar->command();
-    #endif
+	virtual void command()
+	{
+		while (1)
+		{
+			clrscr();
+			int sz = v.size();
+
+			for (int i = 0; i < sz; i++)
+				cout << i + 1 << ". " << v[i]->getTitle() << endl;
+
+			cout << sz + 1 << ". 상위 메뉴로" << endl;
+			//--------------------------------------
+			cout << "메뉴를 선택하세요 >> ";
+			int cmd;
+			cin >> cmd;
+			if (cmd < 1 || cmd >  sz + 1) continue;
+
+			if (cmd == sz + 1) break;
+
+			v[cmd - 1]->command();
+		}
+	}
+};
+
+int main()
+{
+	PopupMenu* menubar = new PopupMenu("MENUBAR");
+
+	PopupMenu* popup1 = new PopupMenu("색상");
+	PopupMenu* popup2 = new PopupMenu("해상도");
+
+	menubar->addMenu(popup1);
+	popup1->addMenu(popup2);
+
+	MenuItem* item1 = new MenuItem("빨강색", 11);
+	MenuItem* item2 = new MenuItem("파란색", 12);
+
+	popup1->addMenu(item1);
+	popup1->addMenu(item2);
+
+	MenuItem* item3 = new MenuItem("1024*768", 21);
+	MenuItem* item4 = new MenuItem("1920*1080", 22);
+
+	popup2->addMenu(item3);
+	popup2->addMenu(item4);
+	//-------------------------------
+	// 최상위 메뉴인 menubar를 선택
+	menubar->command();
+
+	delete menubar;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
